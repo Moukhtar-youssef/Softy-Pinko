304ea6ac8a39f9c3097fbf3ca881d21feddb50a7
Moukhtar-youssef <moukhtar.youssef06@gmail.com>
2022-07-11 02:52:05 +0200

    finishing the website

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
-let menu = document.querySelector(".navigation .container .menu");
+// changing the color of header according to the window scroll
+let sections = document.querySelectorAll("section");
+let links = document.querySelectorAll(".navbar ul li a");
+let menu_links = document.querySelectorAll(".menu ul li a");
+window.addEventListener("scroll", () => {
+  let user_position = document.documentElement.scrollTop;
+  sections.forEach((section) => {
+    if (
+      user_position >= section.offsetTop - 100 &&
+      user_position <= section.offsetHeight + section.offsetTop
+    ) {
+      links.forEach((link) => {
+        if (section.id === link.className.toLocaleLowerCase()) {
+          removeactive(links);
+          setactive(link);
+        }
+      });
+      menu_links.forEach((link) => {
+        if (section.id === link.className.toLocaleLowerCase()) {
+          removeactive(menu_links);
+          setactive(link);
+        }
+      });
+    }
+  });
+});
+function removeactive(links) {
+  links.forEach((element) => {
+    element.classList.remove("active");
+  });
+}
+function setactive(link) {
+  link.classList.add("active");
+}
+// opening the menu with a click
+let menu = document.querySelector(".navbar .container .menu");
 menu.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
@@ -21,6 +56,49 @@ function animate(parent, ...childs) {
     }
   });
 }
+//adding animation
+let about = document.querySelector(".about");
+let about_image_first = document.querySelector(".about .container .first img");
+let about_content_first = document.querySelector(
+  ".about .container .first .content"
+);
+let about_image_second = document.querySelector(
+  ".about .container .second img"
+);
+let about_content_second = document.querySelector(
+  ".about .container .second .content"
+);
+animate(
+  about,
+  about_image_first,
+  about_content_first,
+  about_image_second,
+  about_content_second
+);
+let Work = document.querySelector(".work");
+let work_headding = document.querySelector(".work .heading");
+let work_cards = document.querySelector(".work .work_cards");
+animate(Work, work_headding, work_cards);
+let testimonails = document.querySelector(".testimonials");
+let testimonails_heading = document.querySelector(".testimonials .heading");
+let testimonails_box_first = document.querySelector(".testimonials .first");
+let testimonails_box_second = document.querySelector(".testimonials .second");
+let testimonails_box_third = document.querySelector(".testimonials .third");
+animate(
+  testimonails,
+  testimonails_heading,
+  testimonails_box_first,
+  testimonails_box_second,
+  testimonails_box_third
+);
+let price = document.querySelector(".pricing");
+let price_heading = document.querySelector(".pricing .heading");
+let price_tags = document.querySelector(".pricing .prices");
+animate(price, price_heading, price_tags);
+let blog = document.querySelector(".blog-entries");
+let blog_heading = document.querySelector(".blog-entries .heading");
+let blog_content = document.querySelector(".blog-entries .blogs");
+animate(blog, blog_heading, blog_content);
 // add increasing number when up to specefic offset
 let section = document.querySelector(".numbers");
 let nums = document.querySelectorAll(".number strong");
